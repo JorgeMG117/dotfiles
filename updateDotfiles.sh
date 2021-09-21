19da4e1e414ce04cbed0a9874445a3a0014f3e2c
JorgeMG117 <jorgemartinezgil117@gmail.com>
2021-09-21 22:40:32 +0200

    a

---
+++ updateDotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME status -s | cut -d' ' -f 3 |
+while read line
+do
+    /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add "$line"
+done
